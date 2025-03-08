85423b38fd221bdb277e740226e73af3a60cfdf6
Full-Stack-Job-Portal <ankushsingh1088@gmail.com>
2025-03-09 03:25:20 +0530

    change in code and test API

--- Router/router.user.js
+++ Router/router.user.js
@@ -1,13 +1,16 @@
 import express from "express";
 import { Login, Logout, register, update } from "../Controller/user.controller.js";
 import authentication from "../Middleware/isAuthenticated.js";
-
+import { singleUpload } from "../Middleware/multer.js";
 const router = express.Router(); 
+router.route("/register").post((req, res, next) => {
+  console.log("File Upload Middleware Running");
+  next();
+}, singleUpload, register);
 
-router.route("/register").post(register);
+//router.route("/register").post(singleUpload , register);
 router.route("/login").post(Login); 
 router.route("/Logout").post(Logout); 
 router.route("/profile/update").post(authentication, update);  
 
-export default router;
-  
+export default router; 
--- index.js
+++ index.js
@@ -19,19 +19,22 @@ app.use(cookieParser());
 
 
 const corsOption = {
-  origin: ["http://localhost:5000"],
+  origin: ["http://localhost:5173"],
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"], // ✅ Allow methods
+  allowedHeaders: ["Content-Type", "Authorization"], 
 };
 
 // Use routes (if necessary) 
 app.use(cors(corsOption));
 
-const PORT =  5000;
-;
 app.use('/api/user', userRoute);
 app.use('/api/company', companyRouter);
 app.use('/api/job', jobRouter);
-app.use('./api/application', applicationRoute);
+app.use('/api/application', applicationRoute);
+
+const PORT =  5000;
+
 app.listen(PORT, () => {
   connectDB();
   console.log(`server is start on port number ${PORT}`);
